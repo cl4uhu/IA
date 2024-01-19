6ed634db4afc2c4f2c4a2a38cd594c778f5e8fc4
cl4uhu <pjclaudia1@gmail.com>
2024-01-19 10:52:22 +0100

    ai

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    private NavMeshAgent _playerAgent;
+    void Awake()
+    {
+        _playerAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            SetDestination();
+        }
+    }
+    void SetDestination()
+    {
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            _playerAgent.destination = hit.point;
+        }
+    }
+}
